750f584b66bbdaeba85dc8ce24a490dd3e135ace
akashkharde <akashkharde111@gmail.com>
2025-12-17 02:40:51 +0530

    spelling

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "bakcend",
+      name: "backend",
       script: "./dist/index.js", // TypeScript compiled output
       instances: "max",
       exec_mode: "cluster",
